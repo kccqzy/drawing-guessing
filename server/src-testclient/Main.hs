844ad38bd21317b1d0ff785e2d1dfb0f8f61ca68
Zhouyu Qian <qzy@qzy.io>
2018-05-31 02:19:43 -0700

    Add simplistic client testing program

--- server/src-testclient/Main.hs
+++ server/src-testclient/Main.hs
@@ -1,4 +1,43 @@
-module Main (main) where
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main
+  ( main
+  ) where
+
+import Control.Concurrent.Async
+import Control.Monad
+import Network.URI
+import Network.WebSockets
+import System.Environment
+import System.IO
+import Text.Read (readMaybe)
+import Types
+
+clientApp :: ClientApp ()
+clientApp conn =
+  withAsync (forever $ do
+                l <- getLine
+                let msg = read l :: Msg
+                sendTextData conn msg) $ \_ -> forever $ do
+    (msg :: Msg) <- receiveData conn
+    print msg
+
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  args <- getArgs
+  case args of
+    [nick] ->
+      runClient "127.0.0.1" 8080 ("/new-room?nickname=" ++ escapeURIString isUnescapedInURIComponent nick) clientApp
+    [nick, pinStr]
+      | Just pin <- readMaybe pinStr
+      , minPin <= pin && pin <= maxPin ->
+        runClient
+          "127.0.0.1"
+          8080
+          ("/join-room?nickname=" ++ escapeURIString isUnescapedInURIComponent nick ++ "&room_id=" ++ show pin)
+          clientApp
+    _ -> hPutStrLn stderr "usage: dg-client NICK [PIN]"
+  where
+    minPin, maxPin :: Int
+    minPin = 1000000
+    maxPin = 9999999
